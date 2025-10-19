dad4c633a2c9983f13196f1deb2e3cbadb48eabb
Alejandro Garcia <73711220+Chagc@users.noreply.github.com>
2025-10-19 17:04:21 -0600

    Modifiqué Client para la nube

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ const client = new Client({
   authStrategy: new LocalAuth({ clientId: 'wa-bot' }),
   puppeteer: {
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    executablePath: '/usr/bin/chromium', // 👈 fuerza usar el Chromium del sistema
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--disable-gpu'
+    ]
   }
 });
 
